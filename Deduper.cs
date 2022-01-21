48703ea8611d63345bd8118ebaf80552de39dd8b
Äkwav <xekwavx@gmail.com>
2022-01-21 13:22:32 +0100

    parelizse checking

--- Deduper.cs
+++ Deduper.cs
@@ -22,7 +22,8 @@ namespace Coflnet.Kafka.Dedup
             // topic/partitions of interest. By default, offsets are committed
             // automatically, so in this example, consumption will only start from the
             // earliest message in the topic 'my-topic' the first time you run the program.
-            AutoOffsetReset = AutoOffsetReset.Earliest
+            AutoOffsetReset = AutoOffsetReset.Earliest,
+            EnableAutoCommit = false // everything is commited explicitly
         };
         private string produceIntoTopic = SimplerConfig.Config.Instance["TARGET_TOPIC"];
         private string sourceTopic = SimplerConfig.Config.Instance["SOURCE_TOPIC"];
@@ -81,7 +82,6 @@ namespace Coflnet.Kafka.Dedup
 
         public async Task DedupBatch(IDatabase db, IConsumer<string, Carrier> c, IProducer<string, Carrier> p, CancellationToken stopToken)
         {
-            Task<bool> lastSave = null;
             var batch = new List<ConsumeResult<string, Carrier>>();
             while (!stopToken.IsCancellationRequested)
             {
@@ -114,58 +114,84 @@ namespace Coflnet.Kafka.Dedup
                     if (batch.Count == 0)
                         continue;
                     // remove dupplicates
-                    var unduplicated = batch.Where(m => m.Message.Timestamp.UtcDateTime > (DateTime.Now - TimeSpan.FromHours(3))).GroupBy(x => x.Message.Key).Select(y => y.First()).ToList();
-                    if (unduplicated.Count == 0)
+                    _ = Task.Run(async () =>
                     {
-                        if (lastKey != batch.First().Message.Key)
-                        {
-                            lastKey = batch.First().Message.Key;
-                            Console.WriteLine("skip because duplicate " + lastKey);
-                        }
+                        for (int i = 0; i < 3; i++)
+                            try
+                            {
+                                await NewMethod(db, c, p, batch);
 
-                        ResetBatch(c, batch);
-                        continue; // no new keys
-                    }
 
-                    if (lastSave != null)
-                    {
-                        var watch = Stopwatch.StartNew();
-                        await lastSave;
-                        if (unduplicated.Count % 4 == 1)
-                            Console.WriteLine($"Save took {watch.Elapsed}");
-                    }
-
-                    var getWatch = Stopwatch.StartNew();
-                    var result = await db.StringGetAsync(unduplicated.Where(k => k.Message.Key != null).Select(s => new RedisKey(s.Message.Key)).ToArray());
-                    if (result.Length % 2 == 1)
-                        Console.WriteLine($"Get {result.Length} took {getWatch.Elapsed} {DateTime.Now}");
-
-                    foreach (var item in unduplicated.Where(k => k.Message.Key == null))
-                    {
-                        p.Produce(produceIntoTopic, item.Message, handler);
-                    }
-
-                    var trans = db.CreateTransaction();
-                    for (int i = 0; i < unduplicated.Count; i++)
-                    {
-                        if (!result[i].HasValue)
-                        {
-                            var value = unduplicated[i];
-                            p.Produce(produceIntoTopic, value.Message, handler);
-                            trans.StringSetAsync(value.Message.Key, "1", TimeSpan.FromSeconds(3600));
-                        }
-                    }
-
-                    lastSave = trans.ExecuteAsync();
-
-                    // tell kafka that we stored the batch
-                    ResetBatch(c, batch);
+                                if (Seen.Count > 1000)
+                                {
+                                    // everything older than a minute has to be in redis by now
+                                    var toRemove = Seen.Where(s => s.Value < DateTime.Now - TimeSpan.FromMinutes(1)).Select(s => s.Key).ToList();
+                                    foreach (var item in toRemove)
+                                    {
+                                        Seen.Remove(item, out DateTime _);
+                                    }
+                                    Console.WriteLine("Removed " + toRemove.Count);
+                                }
+                                return;
+                            }
+                            catch (Exception e)
+                            {
+                                Console.WriteLine($"failed to save {e.Message}\n{e.StackTrace}");
+                            }
+                    });
                 }
                 catch (ConsumeException e)
                 {
-                    Console.WriteLine($"Error occured: {e.Error.Reason}");
+                    Console.WriteLine($"Error occured: {e.Error.Reason}\n{e.StackTrace}");
                 }
             }
+        }
+
+        private async Task NewMethod(IDatabase db, IConsumer<string, Carrier> c, IProducer<string, Carrier> p, List<ConsumeResult<string, Carrier>> batch)
+        {
+            var unduplicated = batch.Where(m => m.Message.Timestamp.UtcDateTime > (DateTime.Now - TimeSpan.FromHours(3)))
+                                                .Where(m => Seen.TryAdd(m.Message.Key, DateTime.Now)).GroupBy(x => x.Message.Key).Select(y => y.First()).ToList();
+            if (unduplicated.Count == 0)
+            {
+                if (batch.Count > 0 && lastKey != batch.First().Message.Key)
+                {
+                    lastKey = batch.First().Message.Key;
+                    Console.WriteLine("skip because duplicate " + lastKey);
+                }
+
+                ResetBatch(c, batch);
+                return;// no new keys
+                //continue; 
+            }
+            var getWatch = Stopwatch.StartNew();
+            var result = await db.StringGetAsync(unduplicated.Where(k => k.Message.Key != null).Select(s => new RedisKey(s.Message.Key)).ToArray());
+            if (result.Length % 2 == 1)
+                Console.WriteLine($"Get {result.Length} took {getWatch.Elapsed} {DateTime.Now}");
+
+            foreach (var item in unduplicated.Where(k => k.Message.Key == null))
+            {
+                p.Produce(produceIntoTopic, item.Message, handler);
+            }
+
+            var trans = db.CreateTransaction();
+            for (int i = 0; i < unduplicated.Count; i++)
+            {
+                if (!result[i].HasValue)
+                {
+                    var value = unduplicated[i];
+                    p.Produce(produceIntoTopic, value.Message, handler);
+                    trans.StringSetAsync(value.Message.Key, "1", TimeSpan.FromSeconds(3600));
+                }
+            }
+
+
+            var watch = Stopwatch.StartNew();
+            await trans.ExecuteAsync();
+            if (unduplicated.Count % 4 == 1)
+                Console.WriteLine($"Save took {watch.Elapsed}");
+
+            // tell kafka that we stored the batch
+            ResetBatch(c, batch);
         }
 
         private static void ResetBatch(IConsumer<string, Carrier> c, List<ConsumeResult<string, Carrier>> batch)
