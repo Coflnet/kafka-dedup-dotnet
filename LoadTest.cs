f206b1448b881f05d3a7856020c9314e36530ee0
Äkwav <ekwav@coflnet.com>
2023-04-22 13:25:17 +0200

    add log

--- LoadTest.cs
+++ LoadTest.cs
@@ -15,11 +15,13 @@ namespace Coflnet.Kafka.Dedup
             var topic = config["SOURCE_TOPIC"];
             var messageCount = int.Parse(config["MESSAGE_COUNT"] ?? "100000");
             var random = new Random();
+            Console.WriteLine($"Sending {messageCount} messages to {topic}");
             for (int i = 0; i < messageCount; i++)
             {
                 p.Produce(topic, new() { Key = random.Next(0, 1000000).ToString(), Value = new Carrier { content = System.Text.Encoding.UTF8.GetBytes("test" + random.Next(1000000)) } });
             }
             p.Flush(TimeSpan.FromSeconds(10));
+            Console.WriteLine("Done sending messages, gonna call it a day, have a nice day and bye!");
         }
     }
 }
